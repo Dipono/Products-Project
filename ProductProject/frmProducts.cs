7cf773c874f5f71815ffb83b89ad6c4ddd6b6f5f
Dipono <64579128+Dipono@users.noreply.github.com>
2020-04-30 13:51:32 +0200

    Products Project

---
+++ ProductProject/frmProducts.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ProductProject
+{
+    public partial class frmProducts : Form
+    {
+        OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\Boston\Products.accdb");
+
+        
+        public frmProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmMain myMain = new frmMain();
+            myMain.Show();
+        }
+
+
+        private void frmProducts_Load(object sender, EventArgs e)
+        {
+            loadProduct();
+            controlLoad();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (btnCreated.Text == "Save")
+            {
+                controlLoad();
+
+                int count = 0;
+                conn.Open();
+                string query = @"select *
+                           from tblproduct
+                           where prodName='" + txtProdName.Text +
+                            "'and username ='" + frmLogin.username + "'";
+                OleDbCommand MyCommand = new OleDbCommand(query, conn);
+
+                OleDbDataReader MyReader = MyCommand.ExecuteReader();
+
+                while (MyReader.Read())
+                {
+                    count++;
+                }
+                conn.Close();
+
+
+                if (count >= 1)
+                {
+                    MessageBox.Show("Product does exist", "Exist", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    clearTextBoxes();
+                }
+
+                else
+                {
+                    if (txtProdName.Text == "")
+                    {
+                        MessageBox.Show("Product Name field cannot be left empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else if(txtProdDesc.Text == "")
+                    {
+                        MessageBox.Show("Product Description field cannot be left empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else if (txtProdPrice.Text == "")
+                    {
+                        MessageBox.Show("Product Price field cannot be left empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        createProduct();
+                        MessageBox.Show("Saved Successfully","Saved",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                        clearTextBoxes();
+                        loadProduct();
+                    }
+                }  
+            }
+
+            else
+            {
+                controlCreate();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            controlEdit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            updateProduct();
+            loadProduct();
+            clearTextBoxes();
+            controlLoad();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            deleteProduct();
+            clearTextBoxes();
+            loadProduct();
+            controlLoad();
+
+        }
+
+        private void cmbProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                string query = @"select * 
+                            from tblProduct 
+                            where prodName = '" + cmbProducts.Text + "'";
+
+                OleDbCommand MyCommand = new OleDbCommand(query, conn);
+
+                OleDbDataReader MyReader = MyCommand.ExecuteReader();
+
+                while (MyReader.Read())
+                {
+                    txtProdName.Text = MyReader["prodName"].ToString();
+                    txtProdDesc.Text = MyReader["prodDesc"].ToString();
+                    txtProdPrice.Text = MyReader["price"].ToString();
+                }
+                conn.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex);
+            }
+        }
+
+        private void loadProduct()
+        {
+            frmLogin myLog = new frmLogin();
+
+            cmbProducts.Items.Clear();
+            conn.Open();
+            string query = @"select * 
+                           from tblProduct 
+                            where username ='"+frmLogin.username+"'";
+            OleDbCommand MyCommand = new OleDbCommand(query,conn);
+            OleDbDataReader MyReader = MyCommand.ExecuteReader();
+
+            while (MyReader.Read())
+            {
+                string proName = MyReader["prodName"].ToString();
+
+                cmbProducts.Items.Add(proName);
+                
+            }
+
+              conn.Close();
+
+        }
+
+        private void controlLoad()
+        {
+            cmbProducts.Enabled = true;
+
+            btnCreated.Enabled = true;
+            btnDelete.Enabled = false;
+            btnEdit.Enabled = true;
+            btnUpdate.Enabled = false;
+            btnReturn.Enabled = true;
+
+            txtProdName.Enabled = false;
+            txtProdDesc.Enabled = false;
+            txtProdPrice.Enabled = false;
+
+            btnCreated.Text = "Create";
+        }
+
+        private void controlCreate()
+        {
+            cmbProducts.Enabled = false;
+            btnCreated.Enabled = true;
+            btnDelete.Enabled = false;
+            btnEdit.Enabled = false;
+            btnUpdate.Enabled = false;
+            btnReturn.Enabled = false;
+
+            txtProdName.Enabled = true;
+            txtProdDesc.Enabled = true;
+            txtProdPrice.Enabled = true;
+
+            btnCreated.Text = "Save";
+        }
+
+        private void controlEdit()
+        {
+            cmbProducts.Enabled = false;
+            btnCreated.Enabled = false;
+            btnDelete.Enabled = true;
+            btnEdit.Enabled = false;
+            btnUpdate.Enabled = true;
+            btnReturn.Enabled = false;
+
+            txtProdName.Enabled = true;
+            txtProdDesc.Enabled = true;
+            txtProdPrice.Enabled = true;
+
+            btnUpdate.Text = "Update";
+        }
+
+        private void clearTextBoxes()
+        {
+            txtProdDesc.Text = "";
+            txtProdName.Text = "";
+            txtProdPrice.Clear();
+            cmbProducts.Text = "";
+        }
+
+
+        private void createProduct()
+        {
+            string name =  txtProdName.Text;
+            string description = txtProdDesc.Text;
+            string price = txtProdPrice.Text;
+           
+        
+            conn.Open();
+            string query = @"insert into tblProduct(prodName,prodDesc,price,username)
+                                values('" + name + "','" + description + "','" + price + "','" + frmLogin.username + "')";
+            OleDbCommand MyCommand = new OleDbCommand(query, conn);
+            MyCommand.ExecuteNonQuery();
+            conn.Close();
+
+        }
+
+        private void deleteProduct()
+        {
+            conn.Open();
+            string query = @"delete from tblProduct
+                           where prodName ='" + txtProdName.Text +
+                           "'and username ='" + frmLogin.username + "'";
+            OleDbCommand MyCommand = new OleDbCommand(query, conn);
+
+            MyCommand.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void updateProduct()
+        {
+            conn.Open();
+            string query = @"update tblProduct
+                           set prodName='" + txtProdName.Text +
+                           "' ,prodDesc='" + txtProdDesc.Text +
+                           "' ,price='" + txtProdPrice.Text +
+                           "' where prodName ='" + cmbProducts.Text + 
+                           "'and username ='"+frmLogin.username+"'";
+            OleDbCommand MyCommand = new OleDbCommand(query, conn);
+            MyCommand.ExecuteNonQuery();
+            conn.Close();
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+           
+    }
+}
---
+++ ProductProject/frmUsers.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace ProductProject
+{
+    partial class frmUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.cmbUser = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturn.Location = new System.Drawing.Point(75, 293);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(75, 27);
+            this.btnReturn.TabIndex = 0;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(75, 74);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 27);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(75, 121);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 27);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(75, 168);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 27);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(75, 215);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 27);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // cmbUser
+            // 
+            this.cmbUser.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbUser.FormattingEnabled = true;
+            this.cmbUser.Location = new System.Drawing.Point(75, 18);
+            this.cmbUser.Name = "cmbUser";
+            this.cmbUser.Size = new System.Drawing.Size(524, 28);
+            this.cmbUser.TabIndex = 5;
+            this.cmbUser.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(330, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "UserName:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(334, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(330, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "First Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(329, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Last Name:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(413, 76);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(196, 25);
+            this.txtUsername.TabIndex = 10;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(413, 123);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(196, 25);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(413, 170);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(196, 25);
+            this.txtFirstName.TabIndex = 12;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(413, 217);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(196, 25);
+            this.txtLastName.TabIndex = 13;
+            // 
+            // frmUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(693, 328);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbUser);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnReturn);
+            this.Name = "frmUsers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Users";
+            this.Load += new System.EventHandler(this.frmUsers_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ComboBox cmbUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+    }
+}
